f529d0eb8baf026230bd9365fb2f53a03262fda4
arpipatel-ap <arpipatel96@gmail.com>
2024-06-27 01:40:28 -0400

    Implement function in filetrs.rb

--- filters.rb
+++ filters.rb
@@ -1,16 +1,32 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+require './candidates'
 
 def find(id)
-    # Your code Here
+  @candidates.each{ |candidate| return candidate if candidate[:id] == id }
+  nil
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    qualified = []
+    candidate.each do |candidate|
+      if candidate[:years_of_experience] > 0 &&
+        candidate[:github_points] >= 100 &&
+        (candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")) &&
+        candidate[:age] > 17 &&
+        candidate[:applied_in_last_15_days]
+        qualified << candidate
+        end
+      end
+      qualified
   end
-  
-  # More methods will go below
+
+  def ordered_by_qualifications(candidates)
+
+    candidates.sort_by do|candidate| [-candidate[:years_of_experience], -candidate[:github_points]] 
+    end
+  end
